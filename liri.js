f5c9357e04bf906f580fd24a307376c6a9b68c09
BeardCannon <52046779+nfarrell87@users.noreply.github.com>
2019-08-12 00:08:07 -0700

    updated js

--- liri.js
+++ liri.js
@@ -7,73 +7,98 @@ var axios = require("axios");
 
 var input = process.argv[2];
 var input2 = process.argv.splice(3).join(" ");
-var output;
+
 switch (input) {
     case "concert-this":
-        axios.get("https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp")
-            .then(function (response) {
-                fs.writeFile("concertdata.txt", JSON.stringify(response.data, null, 2), function (err) {
+        var artist = input2
+        var search = '\n\n' + "Keyword(s) used for search: " + artist;
+        if (artist === "") {
+            console.log("Please type in a band you'd like to find concert dates for");
+        } else {
+            fs.appendFile("log.txt", search, function (err) {
 
-                    // If the code experiences any errors it will log the error to the console.
-                    if (err) {
-                        return console.log(err);
+                // If the code experiences any errors it will log the error to the console.
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+                .then(function (response) {
+                    for (i = 0; i < response.data.length; i++) {
+                        var venue = "Venue: " + response.data[i].venue.name + '\n';
+                        var location = "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + '\n';
+                        var date = "Date: " + response.data[i].venue.datetime + '\n';
+                        var concertInfo = venue + location + date;
+                        console.log(concertInfo);
+                        fs.appendFile("log.txt", concertInfo, function (err) {
+
+                            // If the code experiences any errors it will log the error to the console.
+                            if (err) {
+                                return console.log(err);
+                            }
+                        });
+                        console.log("search history was updated!");
                     }
 
-                    // Otherwise, it will print: "movies.txt was updated!"
-                    console.log("concertdata.txt was updated!");
-                    for (i = 0; i < response.data.length; i++)
-                    {
-                        if (i === 0) {
-                            console.log("Venue: " + response.data[i].venue.name, '\n', "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + '\n', + "Date: " + response.data[i].venue.datetime);
-                        } else {
-                            console.log("Venue: " + response.data[i].venue.name, '\n', "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region, '\n', "Date: " + response.data[i].venue.datetime);
-                        }
-                    }
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
                 });
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-        
+        }
         break;
     case "spotify-this-song":
         var spotify = new Spotify(keys.spotify);
         var song = input2;
-
+        if (song === "") {
+            song = "Never gonna give you up"
+        };
         spotify
             .search({ type: 'track', query: song })
             .then(function (response) {
-                fs.writeFile("spotifydata.txt", JSON.stringify(response, null, 2), function (err) {
+                for (var i = 0; i < response.tracks.items[0].artists.length; i++) {
+                    if (i === 0) {
+                        console.log("Artist(s): " + response.tracks.items[0].artists[i].name);
+                        var artist = "Artist(s): " + response.tracks.items[0].artists[i].name + '\n';
+
+                    } else {
+                        console.log(" " + response.tracks.items[0].artists[i].name);
+                    }
+                }
+                console.log("Song: " + response.tracks.items[0].name);
+                console.log("Album: " + response.tracks.items[0].album.name);
+                var preview = response.tracks.items[0].preview_url;
+                if (preview === null) {
+                    console.log("Preview Link: Unavailable for this song :(");
+                    var previewText = "Preview Link: Unavailable on Spotify for this song :(" + '\n';
+                } else {
+                    console.log("Preview Link: " + preview);
+                    var previewText = "Preview Link: " + preview + '\n';
+                }
+                var search = '\n' + "Keyword(s) used for search: " + song + '\n';
+                var songName = "Song: " + response.tracks.items[0].name + '\n';
+                var album = "Album: " + response.tracks.items[0].album.name + '\n';
+
+
+                fs.appendFile("log.txt", search + artist + songName + album + previewText, function (err) {
 
                     // If the code experiences any errors it will log the error to the console.
                     if (err) {
                         return console.log(err);
                     }
-
                     // Otherwise, it will print: "movies.txt was updated!"
-                    console.log("spotifydata.txt was updated!");
-                    for (var i = 0; i < response.tracks.items[0].artists.length; i++) {
-                        if (i === 0) {
-                            console.log("Artist(s): " + response.tracks.items[0].artists[i].name);
-                        } else {
-                            console.log("              " + response.tracks.items[0].artists[i].name);
-                        }
-                    }
-                    console.log("Song: " + response.tracks.items[0].name);
-                    console.log("Preview Link: " + response.tracks.items[0].preview_url);
-                    console.log("Album: " + response.tracks.items[0].album.name);
+                    console.log("search history was updated!");
                 });
-
             })
             .catch(function (err) {
                 console.log(err);
             });
-       
+
         break;
     case "movie-this":
         var movie = input2;
-            axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+        var search = '\n' + "Keyword(s) used for search: " + movie + '\n';
+        axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
             .then(function (response) {
                 console.log("Title of the movie: " + response.data.Title);
                 console.log("Year the movie came out: " + response.data.Year);
@@ -83,12 +108,71 @@ switch (input) {
                 console.log("Language of the movie: " + response.data.Language);
                 console.log("Plot of the movie: " + response.data.Plot);
                 console.log("Actors in the movie: " + response.data.Actors);
-             });
+                var title = "Title of the movie: " + response.data.Title + '\n';
+                var year = "Year the movie came out: " + response.data.Year + '\n';
+                var imdb = "IMDB Rating of the movie: " + response.data.imdbRating + '\n';
+                var meta = "Rotten Tomatoes Rating of the movie: " + response.data.Metascore + '\n';
+                var country = "Country where the movie was produced: " + response.data.Country + '\n';
+                var language = "Language of the movie: " + response.data.Language + '\n';
+                var plot = "Plot of the movie: " + response.data.Plot + '\n';
+                var actors = "Actors in the movie: " + response.data.Actors + '\n';
+                fs.appendFile("log.txt", search + title + year + imdb + meta + country + language + plot + actors, function (err) {
+
+                    // If the code experiences any errors it will log the error to the console.
+                    if (err) {
+                        return console.log(err);
+                    }
+                    // Otherwise, it will print: "movies.txt was updated!"
+                    console.log("search history was updated!");
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
         break;
     case "do-what-it-says":
-        output = "do what it says";
+        fs.readFile("random.txt", "utf8", function (error, data) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return console.log(error);
+            }
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+
+            // We will then re-display the content as an array for later use.
+            console.log(dataArr);
+            if (dataArr[0] === "spotify-this-song") {
+                var spotify = new Spotify(keys.spotify);
+                var song = dataArr[1];
+
+                spotify
+                    .search({ type: 'track', query: song })
+                    .then(function (response) {
+                        for (var i = 0; i < response.tracks.items[0].artists.length; i++) {
+                            if (i === 0) {
+                                console.log("Artist(s): " + response.tracks.items[0].artists[i].name);
+                            } else {
+                                console.log(" " + response.tracks.items[0].artists[i].name);
+                            }
+                        }
+                        console.log("Song: " + response.tracks.items[0].name);
+                        console.log("Album: " + response.tracks.items[0].album.name);
+                        var preview = response.tracks.items[0].preview_url;
+                        if (preview == null) {
+                            console.log("Preview Link: Unavailable for this song");
+                        } else {
+                            console.log("Preview Link: " + preview);
+                        };
+
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            }
+        });
         break;
     default:
-        output = "Not a recognized command";
+        console.log("Not a recognized command! Please try again");
         break;
 }
