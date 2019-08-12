3562e2a5080ef2ba3d2fc60a58b29c7d381dbca8
BeardCannon <52046779+nfarrell87@users.noreply.github.com>
2019-08-12 00:37:18 -0700

    updated js

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var axios = require("axios");
+var moment = require('moment');
 
 
 var input = process.argv[2];
@@ -11,7 +12,7 @@ var input2 = process.argv.splice(3).join(" ");
 switch (input) {
     case "concert-this":
         var artist = input2
-        var search = '\n\n' + "Keyword(s) used for search: " + artist;
+        var search = '\n\n' + "Keyword(s) used for search: " + artist + '\n';
         if (artist === "") {
             console.log("Please type in a band you'd like to find concert dates for");
         } else {
@@ -27,8 +28,10 @@ switch (input) {
                     for (i = 0; i < response.data.length; i++) {
                         var venue = "Venue: " + response.data[i].venue.name + '\n';
                         var location = "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + '\n';
-                        var date = "Date: " + response.data[i].venue.datetime + '\n';
-                        var concertInfo = venue + location + date;
+                        var date = moment(response.data[i].venue.datetime).format("MM/DD/YYYY");
+                        var dateDisplay = "Date: " + date + '\n';
+                        
+                        var concertInfo = venue + location + dateDisplay + '\n';
                         console.log(concertInfo);
                         fs.appendFile("log.txt", concertInfo, function (err) {
 
@@ -37,9 +40,9 @@ switch (input) {
                                 return console.log(err);
                             }
                         });
-                        console.log("search history was updated!");
+                        
                     }
-
+                    console.log("search history was updated!");
                 })
                 .catch(function (error) {
                     // handle error
@@ -97,6 +100,9 @@ switch (input) {
         break;
     case "movie-this":
         var movie = input2;
+        if (movie === "") {
+            movie = "Mr.Nobody"
+        };
         var search = '\n' + "Keyword(s) used for search: " + movie + '\n';
         axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie)
             .then(function (response) {
@@ -139,19 +145,20 @@ switch (input) {
             }
             // Then split it by commas (to make it more readable)
             var dataArr = data.split(",");
-
-            // We will then re-display the content as an array for later use.
-            console.log(dataArr);
             if (dataArr[0] === "spotify-this-song") {
                 var spotify = new Spotify(keys.spotify);
                 var song = dataArr[1];
-
+                if (song === "") {
+                    song = "Never gonna give you up"
+                };
                 spotify
                     .search({ type: 'track', query: song })
                     .then(function (response) {
                         for (var i = 0; i < response.tracks.items[0].artists.length; i++) {
                             if (i === 0) {
                                 console.log("Artist(s): " + response.tracks.items[0].artists[i].name);
+                                var artist = "Artist(s): " + response.tracks.items[0].artists[i].name + '\n';
+        
                             } else {
                                 console.log(" " + response.tracks.items[0].artists[i].name);
                             }
@@ -159,12 +166,27 @@ switch (input) {
                         console.log("Song: " + response.tracks.items[0].name);
                         console.log("Album: " + response.tracks.items[0].album.name);
                         var preview = response.tracks.items[0].preview_url;
-                        if (preview == null) {
-                            console.log("Preview Link: Unavailable for this song");
+                        if (preview === null) {
+                            console.log("Preview Link: Unavailable for this song :(");
+                            var previewText = "Preview Link: Unavailable on Spotify for this song :(" + '\n';
                         } else {
                             console.log("Preview Link: " + preview);
-                        };
-
+                            var previewText = "Preview Link: " + preview + '\n';
+                        }
+                        var search = '\n' + "Keyword(s) used for search: " + song + '\n';
+                        var songName = "Song: " + response.tracks.items[0].name + '\n';
+                        var album = "Album: " + response.tracks.items[0].album.name + '\n';
+        
+        
+                        fs.appendFile("log.txt", search + artist + songName + album + previewText, function (err) {
+        
+                            // If the code experiences any errors it will log the error to the console.
+                            if (err) {
+                                return console.log(err);
+                            }
+                            // Otherwise, it will print: "movies.txt was updated!"
+                            console.log("search history was updated!");
+                        });
                     })
                     .catch(function (err) {
                         console.log(err);
